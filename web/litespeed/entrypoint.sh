90b99897b4ac0b60f2948098826598f626b72a01
tier940 <tsubasa.ito@relightings.com>
2022-06-22 17:00:34 +0900

    test push

--- web/litespeed/entrypoint.sh
+++ web/litespeed/entrypoint.sh
@@ -1,18 +1,12 @@
-#!/bin/bash
-if [ -z "$(ls -A -- "/usr/local/lsws/conf/")" ]; then
-	cp -R /usr/local/lsws/.conf/* /usr/local/lsws/conf/
-fi
-if [ -z "$(ls -A -- "/usr/local/lsws/admin/conf/")" ]; then
-	cp -R /usr/local/lsws/admin/.conf/* /usr/local/lsws/admin/conf/
-fi
-chown 999:999 /usr/local/lsws/conf -R
-chown 999:1000 /usr/local/lsws/admin/conf -R
+#!/usr/bin/env bash
+LSDIR='/usr/local/lsws'
 
+# Run the Server
 /usr/local/lsws/bin/lswsctrl start
 $@
 while true; do
-	if ! /usr/local/lsws/bin/lswsctrl status | grep 'litespeed is running with PID *' > /dev/null; then
-		break
-	fi
-	sleep 60
+  if ! /usr/local/lsws/bin/lswsctrl status | grep 'litespeed is running with PID *' > /dev/null; then
+    break
+  fi
+  sleep 60
 done
