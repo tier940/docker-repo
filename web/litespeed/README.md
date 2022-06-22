14bc66aff9596ccf30ab4739daa41f420e309366
tier940 <tsubasa.ito@relightings.com>
2022-06-22 17:47:45 +0900

    fix litespeed

---
+++ web/litespeed/README.md
@@ -0,0 +1,15 @@
+# litespeed
+
+## Start & Stop
+```bash
+docker run -p 7080:7080 -p 8088:8088 -ti ghcr.io/tier940/litespeed start
+docker run -p 7080:7080 -p 8088:8088 -ti ghcr.io/tier940/litespeed stop
+```
+
+## Panel auth
+- [https://localhost:7080/login.php](https://localhost:7080/login.php)
+    - User: admin
+    - Pass: 123456
+
+## Simple Feature Demos
+- [http://localhost:8088](http://localhost:8088)
--- web/litespeed/entrypoint.sh
+++ web/litespeed/entrypoint.sh
@@ -1,12 +1,21 @@
 #!/usr/bin/env bash
 LSDIR='/usr/local/lsws'
 
-# Run the Server
-/usr/local/lsws/bin/lswsctrl start
-$@
-while true; do
-  if ! /usr/local/lsws/bin/lswsctrl status | grep 'litespeed is running with PID *' > /dev/null; then
-    break
-  fi
-  sleep 60
-done
+
+case "$1" in
+  start)
+    /usr/local/lsws/bin/lswsctrl start
+    while true; do
+      if ! /usr/local/lsws/bin/lswsctrl status | grep 'litespeed is running with PID *' > /dev/null; then
+        break
+      fi
+      sleep 60
+    done
+    ;;
+  stop)
+    /usr/local/lsws/bin/lswsctrl stop
+    ;;
+  *)
+    exec "$@"
+    ;;
+esac
